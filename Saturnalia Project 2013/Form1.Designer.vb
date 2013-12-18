fb6b944e76cfdb3805fd06d5f06d27bff0755bd1
Amir Amhaz <namhaza4@outlook.com>
2013-12-17 21:08:20 -0500

    Committing game code
    Worked on this last weekend

---
+++ Saturnalia Project 2013/Form1.Designer.vb
@@ -0,0 +1,267 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Easy = New System.Windows.Forms.RadioButton()
+        Me.Medium = New System.Windows.Forms.RadioButton()
+        Me.Hard = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 272)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 25)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(592, 244)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(6, 293)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(589, 26)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Player 1"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(6, 372)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(592, 60)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Play"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 15
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Hard)
+        Me.GroupBox1.Controls.Add(Me.Medium)
+        Me.GroupBox1.Controls.Add(Me.Easy)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.RichTextBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(604, 438)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Start"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.ProgressBar1)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(622, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(150, 438)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Control panel"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 18)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Time: 0:00"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(6, 198)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(138, 30)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Pause"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(5, 85)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(139, 23)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(6, 162)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(138, 30)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Restart"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.Label3.Location = New System.Drawing.Point(6, 66)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(141, 16)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Distance left: 1000 m"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 18)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Health: 3 lives"
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(600, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(184, 462)
+        Me.Panel1.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 334)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 18)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Mode"
+        '
+        'Easy
+        '
+        Me.Easy.AutoSize = True
+        Me.Easy.Checked = True
+        Me.Easy.Location = New System.Drawing.Point(60, 332)
+        Me.Easy.Name = "Easy"
+        Me.Easy.Size = New System.Drawing.Size(89, 22)
+        Me.Easy.TabIndex = 5
+        Me.Easy.TabStop = True
+        Me.Easy.Text = "Beginner"
+        Me.Easy.UseVisualStyleBackColor = True
+        '
+        'Medium
+        '
+        Me.Medium.AutoSize = True
+        Me.Medium.Location = New System.Drawing.Point(155, 332)
+        Me.Medium.Name = "Medium"
+        Me.Medium.Size = New System.Drawing.Size(112, 22)
+        Me.Medium.TabIndex = 6
+        Me.Medium.Text = "Intermediate"
+        Me.Medium.UseVisualStyleBackColor = True
+        '
+        'Hard
+        '
+        Me.Hard.AutoSize = True
+        Me.Hard.Location = New System.Drawing.Point(273, 332)
+        Me.Hard.Name = "Hard"
+        Me.Hard.Size = New System.Drawing.Size(71, 22)
+        Me.Hard.TabIndex = 7
+        Me.Hard.TabStop = True
+        Me.Hard.Text = "Expert"
+        Me.Hard.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(784, 462)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Saturnalia Project"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Hard As System.Windows.Forms.RadioButton
+    Friend WithEvents Medium As System.Windows.Forms.RadioButton
+    Friend WithEvents Easy As System.Windows.Forms.RadioButton
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Saturnalia Project 2013/Form1.vb
@@ -0,0 +1,310 @@
+﻿Public Class Form1
+    <System.Runtime.InteropServices.DllImport("User32.dll")>
+    Private Shared Function GetAsyncKeyState(ByVal vkey As System.Windows.Forms.Keys) As Short
+    End Function
+
+    Dim pencil As System.Drawing.Graphics
+    Dim chariot As New Point(0, 250)
+    Dim enemy(4) As Point
+    Dim rndm As New Random
+    Dim counter, health, distance, time(1), speed As Integer
+    Dim birds, clouds, rocks, plane, meteor As Boolean
+    Dim playerName, hitCause As String
+    Dim font1 As New Font("Arial", 13)
+
+    ' This SoundPlayer plays a sound whenever the player fails the game.
+    Private startSoundPlayer = New System.Media.SoundPlayer("C:\Windows\Media\chord.wav")
+    ' This SoundPlayer plays a sound when the player wins the game.
+    Private finishSoundPlayer = New System.Media.SoundPlayer("C:\Windows\Media\tada.wav")
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Load components
+        pencil = Me.CreateGraphics
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Begin game
+        Timer1.Enabled = True
+        playerName = TextBox1.Text
+        GroupBox1.Hide()
+
+        'Reset variables
+        chariot.X =
+        chariot.Y = 250
+        counter = 0
+        If Easy.Checked Then
+            health = 3
+            speed = 8
+        End If
+
+        If Medium.Checked Then
+            health = 2
+            speed = 6
+        End If
+
+        If Hard.Checked Then
+            health = 1
+            speed = 4
+        End If
+        distance = 1000
+        time(0) = 0
+        time(1) = 0
+        birds = False
+        clouds = False
+        rocks = False
+        plane = False
+        meteor = False
+    End Sub
+
+    Private Sub reset(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+        Label2.Text = "Health: 3 lives"
+        Label3.Text = "Distance left: 1000 m"
+        Label4.Text = "Time: 0:00"
+        ProgressBar1.Value = 0
+        pencil.Clear(Me.BackColor)
+        GroupBox1.Show()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        'In game actions
+        drawBackground()
+        drawChariot()
+        drawEnemy()
+        checkHits()
+        updatePanel()
+
+        checkKeys()
+
+        spawnEnemy()
+
+        If counter <= 1000 Then counter += 1 Else counter = 0
+    End Sub
+
+    Public Sub drawChariot()
+        'Character
+        pencil.FillRectangle(Brushes.PeachPuff, chariot.X + 20, chariot.Y - 10, 10, 10)
+        'Chariot
+        pencil.FillRectangle(Brushes.Maroon, chariot.X, chariot.Y, 50, 20)
+        'Sun
+        pencil.FillEllipse(Brushes.Goldenrod, chariot.X - 120, chariot.Y - 50, 100, 100)
+        'Rope
+        pencil.DrawLine(Pens.Brown, chariot.X - 20, chariot.Y + 10, chariot.X, chariot.Y + 10)
+    End Sub
+
+    Public Sub drawBackground()
+        Select Case distance
+            Case 900 To 1000
+                pencil.FillRectangle(Brushes.Black, 0, 0, 600, 500)
+            Case 800 To 900
+                pencil.FillRectangle(Brushes.DarkSlateBlue, 0, 0, 600, 500)
+            Case 700 To 800
+                pencil.FillRectangle(Brushes.DarkViolet, 0, 0, 600, 500)
+            Case 600 To 700
+                pencil.FillRectangle(Brushes.MediumVioletRed, 0, 0, 600, 500)
+            Case 500 To 600
+                pencil.FillRectangle(Brushes.PaleVioletRed, 0, 0, 600, 500)
+            Case 400 To 500
+                pencil.FillRectangle(Brushes.IndianRed, 0, 0, 600, 500)
+            Case 300 To 400
+                pencil.FillRectangle(Brushes.Orange, 0, 0, 600, 500)
+            Case 200 To 300
+                pencil.FillRectangle(Brushes.OrangeRed, 0, 0, 600, 500)
+            Case 100 To 200
+                pencil.FillRectangle(Brushes.LightSkyBlue, 0, 0, 600, 500)
+            Case 0 To 100
+                pencil.FillRectangle(Brushes.SkyBlue, 0, 0, 600, 500)
+        End Select
+
+        'Update distance and time
+        If counter Mod 2 = 0 Then distance -= 1
+        ProgressBar1.Value = 100 - (distance * (100 / 1000))
+        time(0) = 12 - (distance * (12 / 1000))
+        time(1) = 60 - ((distance Mod (83.3)) * (60 / (83.3)))
+
+        If distance = 0 Then
+            Timer1.Stop()
+            MsgBox("Congratulations " & playerName & "!  You have brought light to the land!  Let there be a glorious new year!")
+        End If
+    End Sub
+
+    Public Sub checkKeys()
+        'Check arrow keys
+        If GetAsyncKeyState(Keys.Down) And chariot.Y < 430 Then
+            chariot.Y += speed
+        End If
+        If GetAsyncKeyState(Keys.Up) And chariot.Y > 20 Then
+            chariot.Y -= speed
+        End If
+        If GetAsyncKeyState(Keys.Left) And chariot.X > 0 Then
+            chariot.X -= speed
+        End If
+        If GetAsyncKeyState(Keys.Right) And chariot.X < 550 Then
+            chariot.X += speed
+        End If
+    End Sub
+
+    Public Sub updatePanel()
+        If health = 1 Then Label2.Text = "Health: " & health & " life" Else Label2.Text = "Health: " & health & " lives"
+        Label3.Text = "Distance left: " & distance & " m"
+        If time(1) < 10 Then Label4.Text = "Time: " & time(0) & ":0" & time(1) Else Label4.Text = "Time: " & time(0) & ":" & time(1)
+    End Sub
+
+    Public Sub spawnEnemy()
+        'If counter = 1000 Then
+        Select Case rndm.Next(0, 5)
+            Case 0
+                If birds = False Then
+                    birds = True
+                    enemy(0).X = 700
+                    enemy(0).Y = rndm.Next(20, 400)
+                End If
+            Case 1
+                If clouds = False Then
+                    clouds = True
+                    enemy(1).X = 700
+                    enemy(1).Y = rndm.Next(20, 400)
+                End If
+            Case 2
+                If rocks = False Then
+                    rocks = True
+                    enemy(2).X = 700
+                    enemy(2).Y = rndm.Next(20, 400)
+                End If
+            Case 3
+                If plane = False Then
+                    plane = True
+                    enemy(3).X = 700
+                    enemy(3).Y = rndm.Next(20, 400)
+                End If
+            Case 4
+                If meteor = False Then
+                    meteor = True
+                    enemy(4).X = 700
+                    enemy(4).Y = rndm.Next(20, 400)
+                End If
+        End Select
+        'End If
+    End Sub
+
+    Public Sub drawEnemy()
+        If birds = True Then
+            pencil.FillRectangle(Brushes.Brown, enemy(0).X, enemy(0).Y, 10, 10)
+            pencil.FillRectangle(Brushes.Brown, enemy(0).X + 30, enemy(0).Y - 15, 10, 10)
+            pencil.FillRectangle(Brushes.Brown, enemy(0).X + 30, enemy(0).Y + 15, 10, 10)
+            pencil.FillRectangle(Brushes.Brown, enemy(0).X + 60, enemy(0).Y - 30, 10, 10)
+            pencil.FillRectangle(Brushes.Brown, enemy(0).X + 60, enemy(0).Y + 30, 10, 10)
+            pencil.FillRectangle(Brushes.Brown, enemy(0).X + 90, enemy(0).Y - 45, 10, 10)
+            pencil.FillRectangle(Brushes.Brown, enemy(0).X + 90, enemy(0).Y + 45, 10, 10)
+
+            If enemy(0).X > -100 Then enemy(0).X -= 5 Else birds = False
+        End If
+
+        If clouds = True Then
+            pencil.FillRectangle(Brushes.White, enemy(1).X, enemy(1).Y, 100, 50)
+            pencil.FillRectangle(Brushes.White, enemy(1).X + 20, enemy(1).Y + 30, 100, 50)
+            pencil.FillRectangle(Brushes.White, enemy(1).X + 50, enemy(1).Y - 10, 100, 50)
+            pencil.FillRectangle(Brushes.White, enemy(1).X + 30, enemy(1).Y + 20, 100, 50)
+
+            If enemy(1).X > -250 Then enemy(1).X -= 3 Else clouds = False
+        End If
+
+        If rocks = True Then
+            pencil.FillRectangle(Brushes.SaddleBrown, enemy(2).X, enemy(2).Y, 50, 50)
+            pencil.FillRectangle(Brushes.SaddleBrown, enemy(2).X + 70, enemy(2).Y - 30, 50, 50)
+            pencil.FillRectangle(Brushes.SaddleBrown, enemy(2).X + 40, enemy(2).Y + 60, 50, 50)
+            pencil.FillRectangle(Brushes.SaddleBrown, enemy(2).X + 110, enemy(2).Y + 30, 50, 50)
+
+            If enemy(2).X > -500 Then enemy(2).X -= 4 Else rocks = False
+        End If
+
+        If plane = True Then
+            pencil.FillRectangle(Brushes.LightSlateGray, enemy(3).X, enemy(3).Y, 200, 50)
+            pencil.FillRectangle(Brushes.LightSlateGray, enemy(3).X + 80, enemy(3).Y - 50, 50, 150)
+            pencil.FillRectangle(Brushes.LightSlateGray, enemy(3).X + 180, enemy(3).Y - 20, 30, 90)
+            pencil.FillRectangle(Brushes.LightSlateGray, enemy(3).X + 180, enemy(3).Y + 10, 50, 30)
+            pencil.DrawString("Time Travel Inc.", font1, Brushes.Black, enemy(3).X + 30, enemy(3).Y + 15)
+
+            If enemy(3).X > -800 Then enemy(3).X -= 2 Else plane = False
+        End If
+
+        If meteor = True Then
+            pencil.FillRectangle(Brushes.DarkGray, enemy(4).X, enemy(4).Y, 200, 200)
+            pencil.FillRectangle(Brushes.OrangeRed, enemy(4).X + 50, enemy(4).Y + 30, 50, 50)
+            pencil.FillRectangle(Brushes.Orange, enemy(4).X + 120, enemy(4).Y + 50, 50, 50)
+            pencil.FillRectangle(Brushes.Yellow, enemy(4).X + 60, enemy(4).Y + 100, 50, 50)
+
+            If enemy(4).X > -1000 Then enemy(4).X -= 1 Else meteor = False
+        End If
+    End Sub
+
+    Public Sub checkHits()
+        If birds = True Then
+            If chariot.X + 25 > enemy(0).X And chariot.X + 25 < enemy(0).X + 90 And chariot.Y + 10 > enemy(0).Y - 45 And chariot.Y + 10 < enemy(0).Y + 45 Then
+                health -= 1
+                birds = False
+                hitCause = "birds"
+            End If
+        End If
+
+        If clouds = True Then
+            If chariot.X + 25 > enemy(1).X And chariot.X + 25 < enemy(1).X + 150 And chariot.Y + 10 > enemy(1).Y - 10 And chariot.Y + 10 < enemy(1).Y + 80 Then
+                health -= 1
+                clouds = False
+                hitCause = "clouds"
+            End If
+        End If
+
+        If rocks = True Then
+            If chariot.X + 25 > enemy(2).X And chariot.X + 25 < enemy(2).X + 160 And chariot.Y + 10 > enemy(2).Y - 30 And chariot.Y + 10 < enemy(2).Y + 110 Then
+                health -= 1
+                rocks = False
+                hitCause = "rocks"
+            End If
+        End If
+
+        If plane = True Then
+            If chariot.X + 25 > enemy(3).X And chariot.X + 25 < enemy(3).X + 230 And chariot.Y + 10 > enemy(3).Y - 50 And chariot.Y + 10 < enemy(3).Y + 50 Then
+                health -= 1
+                plane = False
+                hitCause = "plane"
+            End If
+        End If
+
+        If meteor = True Then
+            If chariot.X + 25 > enemy(4).X And chariot.X + 25 < enemy(4).X + 200 And chariot.Y + 10 > enemy(4).Y And chariot.Y + 10 < enemy(4).Y + 200 Then
+                health -= 1
+                meteor = False
+                hitCause = "meteor"
+            End If
+        End If
+
+        If health <= 0 Then
+            'End game
+            Timer1.Stop()
+            Select Case hitCause
+                Case "birds"
+                    MsgBox(playerName & " was pecked to death by birds!")
+                Case "clouds"
+                    MsgBox(playerName & " was blinded by clouds!")
+                Case "rocks"
+                    MsgBox(playerName & " was pummeled by rocks!")
+                Case "plane"
+                    MsgBox(playerName & " crashed into a plane!")
+                Case "meteor"
+                    MsgBox(playerName & " was struck by a meteor!")
+            End Select
+            MsgBox("The land was thrown into eternal darkness!  Oh no!" & vbNewLine & "Press the restart button to try again.")
+        End If
+    End Sub
+
+    Private Sub Pause(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If Button3.Text = "Pause" Then
+            Timer1.Stop()
+            Button3.Text = "Unpause"
+        ElseIf Button3.Text = "Unpause" Then
+            Timer1.Start()
+            Button3.Text = "Pause"
+        End If
+    End Sub
+End Class
